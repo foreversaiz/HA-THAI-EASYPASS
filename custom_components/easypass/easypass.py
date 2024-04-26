4b4ea79db6ad6a2d19b08856a136e3f7f74c017a
Sirakorn Lamyai <sirakorn.lamyai@adastragrp.com>
2024-04-26 17:07:32 +0700

    isort, black

--- custom_components/easypass/easypass.py
+++ custom_components/easypass/easypass.py
@@ -1,46 +1,44 @@
 import logging
 import random
-from .api import LoginEasyPass
+
 import requests
 
+from .api import LoginEasyPass
 
 
 class EasyPassInstance:
-
-
     def __init__(self, value: str) -> None:
         self._sensor = value
         self._attr_native_value = None
 
     @property
     def value(self):
-        self._offset = self._sensor["offset"] 
+        self._offset = self._sensor["offset"]
         self._offset = int(self._offset) - 1
         with requests.session() as session:
-            LoginEasyPass.login_easypass(session,self._sensor)
+            LoginEasyPass.login_easypass(session, self._sensor)
             cards = LoginEasyPass.get_easypass(session)
-            if cards == "Login Failed" :
-                return cards , ""
-            else :
+            if cards == "Login Failed":
+                return cards, ""
+            else:
                 try:
                     attr = []
-                    attr = { "ทะเบียนรถ": cards[self._offset]['ทะเบียนรถ'], "เลขสมาร์ทการ์ด": cards[self._offset]['เลขสมาร์ทการ์ด (S/N)']}
-                    balance_value = cards[self._offset]['จำนวนเงิน']
+                    attr = {
+                        "ทะเบียนรถ": cards[self._offset]["ทะเบียนรถ"],
+                        "เลขสมาร์ทการ์ด": cards[self._offset]["เลขสมาร์ทการ์ด (S/N)"],
+                    }
+                    balance_value = cards[self._offset]["จำนวนเงิน"]
                     balance_value = balance_value.replace(",", "")
-                    return balance_value , attr
+                    return balance_value, attr
                 except:
                     length = len(cards)
                     attr = []
-                    #attr = { "offset": self._sensor["offset"], "ควรตั้งoffsetเป็น": length}
-                    balance_value = cards[0]['จำนวนเงิน']
+                    # attr = { "offset": self._sensor["offset"], "ควรตั้งoffsetเป็น": length}
+                    balance_value = cards[0]["จำนวนเงิน"]
                     balance_value = balance_value.replace(",", "")
-                    attr = { "license_plate": cards[0]['ทะเบียนรถ'], "balance": balance_value , "smartcard": cards[0]['เลขสมาร์ทการ์ด (S/N)'] }
-                    return cards , attr
-        
-
-
-
-
-
-
-
+                    attr = {
+                        "license_plate": cards[0]["ทะเบียนรถ"],
+                        "balance": balance_value,
+                        "smartcard": cards[0]["เลขสมาร์ทการ์ด (S/N)"],
+                    }
+                    return cards, attr
--- custom_components/easypass/sensor.py
+++ custom_components/easypass/sensor.py
@@ -2,27 +2,24 @@
 from __future__ import annotations
 
 import logging
-
-from .easypass import EasyPassInstance
-import voluptuous as vol
-
+from datetime import timedelta
 from pprint import pformat
-
-from homeassistant.components.sensor import (
-    SensorDeviceClass,
-    SensorEntity,
-    SensorStateClass,
-    PLATFORM_SCHEMA,
-)
 
 # Import the device class from the component that you want to support
 import homeassistant.helpers.config_validation as cv
-from homeassistant.const import CONF_NAME , CONF_USERNAME , CONF_PASSWORD , CONF_OFFSET 
+import voluptuous as vol
+from homeassistant.components.sensor import (
+    PLATFORM_SCHEMA,
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.const import CONF_NAME, CONF_OFFSET, CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-from datetime import timedelta
 
+from .easypass import EasyPassInstance
 
 _LOGGER = logging.getLogger("easypass")
 
@@ -37,15 +34,14 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 
-
 async def async_setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
     add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None
+    discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     """Set up the EasyPass Sensor platform."""
-    #_LOGGER.info(pformat(config))
+    # _LOGGER.info(pformat(config))
 
     username = config[CONF_USERNAME]
     password = config[CONF_PASSWORD]
@@ -55,23 +51,23 @@ async def async_setup_platform(
         "username": config[CONF_USERNAME],
         "password": config[CONF_PASSWORD],
     }
-    
-    add_entities([EasyPassSensor(sensor)],True)
+
+    add_entities([EasyPassSensor(sensor)], True)
+
 
 class EasyPassSensor(SensorEntity):
     """Representation of an EasyPass Sensor."""
 
-
     def __init__(self, sensor) -> None:
         """Initialize an EasyPass Login."""
         _LOGGER.info(pformat(sensor))
-        
+
         self._name = sensor["name"]
         self._attr_native_value = None
         self._value = EasyPassInstance(sensor)
         self.extra_state_attributes = None
         self._attr_unique_id = sensor["name"]
-        
+
     @property
     def name(self) -> str:
         """Return the display name of this EasyPass."""
@@ -82,11 +78,8 @@ class EasyPassSensor(SensorEntity):
         """Return the unit of measurement."""
         return "THB"
 
-
-
-
     def update(self) -> None:
-        #_LOGGER.info(self._value.value)
-        _state , _attr = self._value.value
+        # _LOGGER.info(self._value.value)
+        _state, _attr = self._value.value
         self._attr_native_value = _state
         self.extra_state_attributes = _attr
